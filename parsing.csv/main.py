3095db651f57247f59d1947b0219cae0fd71bf43
bytesandbots <students@bytesandbots.com>
2023-12-22 19:48:14 -0800

    Parsing finished

--- parsing.csv/main.py
+++ parsing.csv/main.py
@@ -1,7 +1,44 @@
 file1 = open("Financials.csv", "r")
 contents = file1.readlines()
 file1.close()
+total = {}
+lineNum = 0
 for line in contents:
-    info = line.split(",")
-    if info[2] == 
-                
+    lineNum += 1
+    if lineNum == 1:
+        continue
+    pos = 0
+    newline = ""
+    for item in line:
+        if(item == ","):
+            before = line[pos-1]
+            after = line[pos+1]
+            date = line[pos-5]
+            if(date == "/"):
+                newline += item
+                continue
+            try: 
+                before = int(before)
+                after = int(after)
+            except:
+                newline += item
+        else:
+            newline += item
+        pos += 1
+    info = newline.split(",")
+    fixed = info[7].replace("$", "")
+    fixed = fixed.replace(" ", "")
+    fixed = fixed.replace('"', "")
+    if info[1] in total:
+        years = total[info[1]]
+        if info[15].strip() in years:
+            years[info[15].strip()] += float(fixed)
+        else:
+            years[info[15].strip()] = float(fixed)
+    else:
+        total[info[1]] = {info[15].strip():float(fixed)}
+for country in total:
+    print(f"For {country}")
+    for year in total[country]:
+        print(f"In {year}")
+        print(f"The total is ${total[country][year]}0")
