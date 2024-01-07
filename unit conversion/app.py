32d83acc8bc488aa7b28107d13931076a9acd162
bytesandbots <students@bytesandbots.com>
2024-01-07 15:03:40 -0800

    Update app.py

--- unit conversion/app.py
+++ unit conversion/app.py
@@ -8,6 +8,7 @@ def conversion():
         unit1 = flask.request.form.get('unitOne')
         unit2 = flask.request.form.get('unitTwo')
         value = flask.request.form.get('firstValue')
+        result = conversion.convert(unit1, unit2, value)
         return "{} is equal to {} {} {}".format(unit1, value, result, value)
     return flask.render_template("index.html")
     
