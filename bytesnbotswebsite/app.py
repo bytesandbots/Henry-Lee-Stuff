d91e07ef1ea0b782b11182bd35072dea8ba9d87f
primboi <students@bytesandbots.com>
2024-01-14 15:03:17 -0800

    botsnbytes

---
+++ bytesnbotswebsite/app.py
@@ -0,0 +1,16 @@
+from flask import render_template, url_for, redirect, Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/<name>")
+def subPage(name)
+    return render_template("url_for(f""{name}.html)")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
